7d5b9e08783cd1b76b66c37f7abb4d4071256cff
Anwesh43 <anweshthecool0@gmail.com>
2018-08-03 10:35:11 +0530

    adding LinkedCircularBeadView

--- app/src/main/java/com/anwesh/uiprojects/linkedcircularbeadview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcircularbeadview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedcircularbeadview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.circularbeadview.CircularBeadView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircularBeadView.create(this)
     }
 }
