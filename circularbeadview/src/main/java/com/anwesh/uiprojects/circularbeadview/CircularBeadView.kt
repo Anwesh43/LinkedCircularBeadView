21817355209f9af43f9336cf01cf3fa8da73d4e8
Anwesh43 <anweshthecool0@gmail.com>
2018-08-03 09:58:34 +0530

    adding LinkedCircularBead class

--- circularbeadview/src/main/java/com/anwesh/uiprojects/circularbeadview/CircularBeadView.kt
+++ circularbeadview/src/main/java/com/anwesh/uiprojects/circularbeadview/CircularBeadView.kt
@@ -151,4 +151,27 @@ class CircularBeadView (ctx : Context) : View(ctx) {
         }
     }
 
+    data class LinkedCircularBead(var i : Int) {
+
+        var curr : CBNode = CBNode(0)
+
+        var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
