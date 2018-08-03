13eb81f84d2916ad3ee952c4d60446a6352e68bb
Anwesh43 <anweshthecool0@gmail.com>
2018-08-03 09:02:51 +0530

    importing libraries, defining nodes and configuring kotlin

---
+++ circularbeadview/src/main/java/com/anwesh/uiprojects/circularbeadview/CircularBeadView.kt
@@ -0,0 +1,13 @@
+package com.anwesh.uiprojects.circularbeadview
+
+/**
+ * Created by anweshmishra on 03/08/18.
+ */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
